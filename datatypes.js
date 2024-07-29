094c48994f6e1f320ce58eb4842b8c6e3a0630bb
Nandini <n.mohanrao.ingole@accenture.com>
2024-07-29 18:50:10 +0530

    file new place

---
+++ datatypes.js
@@ -0,0 +1,44 @@
+//Primitive Data types
+
+// 7 types - String , Number, Boolean, null, undefined, symbol, BigInt
+
+//Referance type/ Non Primitive data types
+
+//Arrays, Objects, Functions
+
+const id=Symbol("123abc");
+const varid=Symbol("123abc");
+
+console.log(id===varid);
+
+const bigNumner=12562374573517n
+
+console.log(typeof bigNumner)
+
+const heros=["Shaktiman" , "Spiderman", "Batman"]//Array
+
+/*let myObj{
+    name="nanidni",
+    age=30,
+    location="Pune"
+}
+
+const myFunstion= funstion1(
+    console.log("hello guys");
+
+)*/
+
+
+//*********************************************************************************************************************************************
+//Memory
+//********************************************************************************************************************************************
+
+
+
+//two type sof memory--Stack and heap 
+// Stack memory - premitive datatypes ********* heap memory -Nonprimitive datatypes
+
+let myValue="nandiniVeer"
+let value2=myValue;
+
+console.log(value2);
